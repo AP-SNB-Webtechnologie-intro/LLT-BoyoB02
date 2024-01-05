ee636feb62a9873363ab7058cf1656036b66c19a
BoyoB02 <145460756+BoyoB02@users.noreply.github.com>
2024-01-05 14:20:20 +0100

    Actie-Topic
    Topic pagina voor Actie Afgewerkt. Hierin zit een tekst, tabel en afbeelding. Voor css heb ik een extra style toegevoegd voor table. hierdoor hebben tabellen een border een kleine padding.

--- Opdracht3/js/mapscript.js
+++ Opdracht3/js/mapscript.js
@@ -1,12 +1,13 @@
-
+//Map Met coordinaten
 let map = L.map('KineMap').setView([51.2460256, 4.4158764], 17);
 
+//Krediet voor OpenStreetMap
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-
+//Marker op map
 let marker = L.marker([51.2460256, 4.4158764], { title: "Kinepolis Antwerpen"}).addTo(map);
 marker.bindPopup("<strong>Kinepolis Antwerpen</strong><br>Groenendaallaan 394").openPopup();
 
